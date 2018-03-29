e642a8ff83ca44a66290ff27687a5f57f4c0282f
James Lotruglio <jimlotruglio@yahoo.com>
2018-03-29 18:07:16 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ end
 def match(words)
   new_array= []
 split_name = @name.split("")
-split_words = words.split(/[" "]+/)
+split_words = words.split(/[" ,"]+/)
 split_words.each do |word|
   split_letters = word.split("")
   if split_name.sort == split_letters
