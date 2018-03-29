47fc6d620153ab20e5ea03924e6744c71611192e
James Lotruglio <jimlotruglio@yahoo.com>
2018-03-29 18:09:10 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,8 +10,7 @@ end
 def match(words)
   new_array= []
 split_name = @name.split("")
-split_words = words.split(/[" ,"]+/)
-split_words.each do |word|
+words.each do |word|
   split_letters = word.split("")
   if split_name.sort == split_letters
     new_array<<split_letters.join
