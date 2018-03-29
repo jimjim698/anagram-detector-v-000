e720a6526b932e43347f9371f343b8effe038066
James Lotruglio <jimlotruglio@yahoo.com>
2018-03-29 18:02:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,25 @@
 # Your code goes here!
 
 class Anagram
+  attr_accessor :name
+
+  initialize(word)
+  @name = word
+end
+
+def match(words)
+  new_array= []
+split_name = @name.split("")
+split_words = words.split(/[" "]+/)
+split_words.each do |word|
+  split_letters = word.split("")
+  if split_name.sort == split_letters
+    new_array<<split_letters.join
+  end
+end
+new_array
   
+end
 
 
 end
